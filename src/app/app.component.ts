a30dc13e79fa3171f74222e84aadee8c8618bebe
Marcelo Bressan <mbressan06@hotmail.com>
2019-11-14 14:16:10 -0300

    Poke type toggle function created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
-  quantity: 7;
   pokesTabs1: any;
   pokesTabs2: any;
-  title = 'pokeapi';
   pokesGen1;
   pokesGen2;
-  pokeSpecie;
-  pokeSprite;
   
   constructor(private apiService: ApiService) {
-    
     this.pokesTabs1 = this.pokesGen1;
     this.pokesTabs2 = this.pokesGen2;
-    
-
   }
   ngOnInit() {
     this.apiService.getGen1().subscribe((data)=>{
       this.pokesGen1 = data['pokemon_species'].slice(0,7);
-      
     });
     this.apiService.getGen2().subscribe((data)=>{
       this.pokesGen2 = data['pokemon_species'].slice(0,7);
-      
     });
-    this.apiService.getSpecie('149').subscribe((data)=>{
-      //console.log('SPECIE: ');
-      //console.log(data);
-      this.pokeSpecie = data;
-      
-    });
-    this.apiService.getSpriteImg('149').subscribe((data)=>{
-      //console.log('SPRITE: ');
-      console.log(data);
-      this.pokeSprite = data['sprites'];
-      
-    });
-
   }
 }
--- src/app/pokes/pokes.component.ts
+++ src/app/pokes/pokes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'card-pokes',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokes.component.scss']
 })
 export class PokesComponent implements OnInit {
+  @Input()
+  pokeId: string;
+  defaultImage;
+  shinyImage
+  pokeName;
+  pokeJpName;
+  genus;
+  flavorText;
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    this.apiService.getSpecie(this.pokeId).subscribe((data)=>{
+      let flavorTextIndex = data['flavor_text_entries'].map(a => a['language']['name']).indexOf("en");
+      this.pokeName = data['names']['2']['name'];
+      this.pokeJpName = data['names']['1']['name'];
+      this.genus = data['genera']['2']['genus'];
+      this.flavorText = data['flavor_text_entries'][flavorTextIndex]['flavor_text'];
+    });
+    this.apiService.getSpriteImg(this.pokeId).subscribe((data)=>{
+      this.defaultImage = data['sprites']['front_default'];
+      this.shinyImage = data['sprites']['front_shiny'];
+  
+    });
   }
+
+  public toggleImage(link : string){
+    document.getElementById('img-poke').src=link;
+  }
+
 
 }
